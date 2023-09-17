c31cf24752ac06bb8c3472ced2d401ee1db21bc9
AhmadAlketnani <ahmadalktnany383@gmail.com>
2023-09-17 23:10:48 +0300

    add new type of routes in project

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// the get routes type
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/send', function () {
-    return '<form action="save" method="post">
-    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
-    <input type="submit" value="send"/>
-    </form>';
-});
-
-Route::post( '/save', function() {
-    return '<h1 >I Get The Massage </h1>';
-});
-
 
 Route::get('/test', function () {
     return '<h1>this the first router with laravel  </h1>';
@@ -37,8 +28,121 @@ Route::get('/test2{name}', function ($name) {
     return '<h1>this the first router with laravel '. $name .' </h1>';
 });
 
-Route::get('users/{id}', function ($id) {
+// *****************************************************************************
 
+// the Post routes type
+
+Route::get('/sendPost', function () {
+    return '<form action="save" method="post">
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="send"/>
+    </form>';
 });
+
+Route::post( '/save', function() {
+    return '<h1 >I Get The Post Massage </h1>';
+});
+
+// *****************************************************************************
+
+// the Put routes type
+
+Route::get('/sendPut', function () {
+    return '<form action="myput" method="post">
+    <input type="hidden" name="_method" value="put"/>
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="put"/>
+    </form>';
+});
+
+Route::put('myput', function () {
+    return '<h1 >I Get The Put Massage </h1>';
+});
+
+// *****************************************************************************
+
+// the Patch routes type
+
+Route::get('/sendPatch', function () {
+    return '<form action="mypatch" method="post">
+    <input type="hidden" name="_method" value="patch"/>
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="patch"/>
+    </form>';
+});
+
+Route::patch( '/mypatch', function() {
+    return '<h1 >I Get The patch Massage </h1>';
+});
+
+// *****************************************************************************
+
+// the Delete routes type
+
+Route::get('/sendDelete', function () {
+    return '<form action="mydelete" method="post">
+    <input type="hidden" name="_method" value="delete"/>
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="delete"/>
+    </form>';
+});
+
+Route::delete( '/mydelete', function() {
+    return '<h1 >I Get The Delete Massage </h1>';
+});
+
+// *****************************************************************************
+
+// the Options routes type
+
+Route::get('/sendOptions', function () {
+    return '<form action="myoptions" method="post">
+    <input type="hidden" name="_method" value="options"/>
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="options"/>
+    </form>';
+});
+
+Route::options( '/myoptions', function() {
+    return '<h1 >I Get The Options Massage </h1>';
+});
+
+// *****************************************************************************
+
+// the Any routes type
+
+Route::get('/sendAny', function () {
+    return '<form action="myany" method="post">
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="any"/>
+    </form>';
+});
+
+
+Route::any( '/myany', function() {
+    return '<h1 >I Get The any Massage </h1>';
+});
+
+// *****************************************************************************
+
+// the Match routes type
+
+Route::get('/sendMatch', function () {
+    return '<form action="mymatch" method="post">
+    <input type="hidden" name="_method" value="delete"/>
+    <input type="hidden" name="_token" value="'. csrf_token() .'"/>
+    <input type="submit" value="match"/>
+    </form>';
+});
+
+
+Route::match( ['delete' , 'post' , 'get'],'/mymatch', function() {
+    return '<h1 >I Get The match Massage </h1>';
+});
+
+// *****************************************************************************
+
+
+
 
 
